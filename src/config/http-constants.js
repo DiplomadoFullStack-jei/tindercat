091b87ba2a2a64273e031ec98b70c2ef7bdee8d8
JEISSON <jibarguen@suyo.co>
2020-07-11 05:58:13 -0500

    Despliegue en RepoDiplomado

---
+++ src/config/http-constants.js
@@ -0,0 +1,11 @@
+export const HTTP_CONSTANTS = {
+    url: 'http://localhost:5001',
+    register: '/cats/signup',
+    login: '/cats/login',
+    autologin: '/cats/autologin',
+    interests: '/interests',
+    updatePreferences: '/cats/preferences',
+    catList: '/cats/',
+    liked: '/cats/Liked',
+    unliked: '/cats/Unliked'
+}
